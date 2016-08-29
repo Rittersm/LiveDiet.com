4f7f598884fc0ea612c730776f8d279ae2b11a73
rittersm <rittersm@gmail.com>
2016-08-29 13:30:25 -0400

    fixed mailer folder name

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,7 +2,7 @@ class UserMailer < ApplicationMailer
 
   def signup_email(user)
     @user = user
-    mail( :to => @user.email, :subject => 'Thanks for joining Chirp' )
+    mail( :to => @user.email, :subject => 'Welcome to LiveDiet' )
   end
 
   def checkin_reminder(user)
